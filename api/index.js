2b1316f0a7d873325c665bdf0228ee7667b09962
Abhishek Tiwari <abhishektiwari290699@gmail.com>
2024-05-10 01:50:34 +0530

    create User model

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 const PORT = 3000;
 
